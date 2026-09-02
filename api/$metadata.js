e15fe629861764816358e2e7a273d54887446c73
instlk <hello.inst.lk@gmail.com>
2026-09-03 03:47:22 +0530

    Update $metadata.js

--- api/$metadata.js
+++ api/$metadata.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.redirect(
     302,
-    "https://appsheet-vrp-test.vercel.app/api/redirect-target"
+    "https://appsheet-vrp-target.vercel.app/api/target"
   );
 }
