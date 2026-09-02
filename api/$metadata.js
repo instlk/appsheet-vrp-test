ad10c99c1883a14458fd31b57cbd4156fd185e4d
instlk <hello.inst.lk@gmail.com>
2026-09-03 04:50:29 +0530

    Update $metadata.js

--- api/$metadata.js
+++ api/$metadata.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
   res.redirect(
     302,
-    "https://appsheet-test.kawinduwijewardhane.co.uk/api/target"
+    "https://appsheetiptest.kawinduwijewardhane.co.uk/api/openapi"
   );
 }
